e349d968f1786b426baa720fbfd92b3dbf63a12d
Gabriela Sanches <gabi@toki.life>
2022-01-21 10:45:56 -0300

    WIP

--- libs/atqr/domain/src/lib/Email-Service/email-service.ts
+++ libs/atqr/domain/src/lib/Email-Service/email-service.ts
@@ -15,12 +15,18 @@ export class ChallengeStarted extends Email {
   to: string; // It ill be type playerEmail from class Player
   subject: 'Seu desafio do ATQR começou !';
   message: 'Text of welcome to the game';
+  from: string //atqr email
+
+  constructor (to: string, subject: string, message: string, from: string){
+      super();
+  }
 }
 
 export class SupConfirmation extends Email {
   to: string; // It ill be type SupervisorEmail from class Player
   subject: 'Você foi convidado a ser supervisor de ${playerName}...';
   message: 'Explicação do jogo';
+  
 }
 
 export class PayThePrice extends Email {
