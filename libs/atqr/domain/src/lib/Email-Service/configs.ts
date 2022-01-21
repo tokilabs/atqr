3fed696918de1b11e85a4a63227d40b63555fdb4
Gabriela Sanches <gabi@toki.life>
2022-01-21 12:38:46 -0300

    WIP
    logic of emailService

---
+++ libs/atqr/domain/src/lib/Email-Service/configs.ts
@@ -0,0 +1,8 @@
+class Configs {
+  public host = '';
+  public port = 587;
+  public user = '';
+  public password = '';
+}
+
+export default new Configs();
--- libs/atqr/domain/src/lib/Email-Service/email-service.ts
+++ libs/atqr/domain/src/lib/Email-Service/email-service.ts
@@ -1,3 +1,6 @@
+import * as nodemailer from 'nodemailer';
+import configs from './configs';
+
 export class EmailService {
   email: string; // it ill be type playerEmail | supervisorEmail (?)
   player: string; // ill be type Player from class  Player
@@ -9,16 +12,48 @@ export class Email {
     public subject?: string,
     public message?: string
   ) {}
+
+  sendMail() {
+    let mailOptions = {
+      from: '',
+      to: this.to,
+      subject: this.subject,
+      html: this.message,
+    };
+
+    const transporter = nodemailer.createTransport({
+      host: configs.host,
+      port: configs.port,
+      secure: false,
+      auth: {
+        user: configs.user,
+        pass: configs.password,
+      },
+      tls: { rejectUnauthorized: false },
+    });
+
+    console.log(mailOptions);
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        return error;
+      } else {
+        return 'E-mail enviado com sucesso!';
+      }
+    });
+  }
 }
+
+export default new Email();
 
 export class ChallengeStarted extends Email {
   to: string; // It ill be type playerEmail from class Player
   subject: 'Seu desafio do ATQR começou !';
   message: 'Text of welcome to the game';
-  from: string //atqr email
+  from: string; //atqr email
 
-  constructor (to: string, subject: string, message: string, from: string){
-      super();
+  constructor(to: string, subject: string, message: string, from: string) {
+    super();
   }
 }
 
@@ -26,7 +61,6 @@ export class SupConfirmation extends Email {
   to: string; // It ill be type SupervisorEmail from class Player
   subject: 'Você foi convidado a ser supervisor de ${playerName}...';
   message: 'Explicação do jogo';
-  
 }
 
 export class PayThePrice extends Email {
