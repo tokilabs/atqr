3c9a56e705222c95ae332ac700c8c9e3516da0d0
Angelica <angelica@toki.life>
2022-01-26 15:54:10 -0300

    WIP player

--- libs/atqr/domain/src/lib/player.ts
+++ libs/atqr/domain/src/lib/player.ts
@@ -1,32 +1,24 @@
-import { Guid } from 'guid-typescript';
-
+import { Guid } from "@tokilabs/lang";
 export class Player{
-  #id: Guid;
-  #name: string;
-  #email: string;
-  #challenges: [];
 
-  get id() {
-    return this.#id;
+
+  constructor(private id: Guid, private name: string, private  email: string, private challenges:challenge[]) {}
+
+  get _id() {
+    return this.id;
   }
 
-  get name() {
-    return this.#name;
+  get _name() {
+    return this.name;
   }
 
-  get email() {
-    return this.#email;
+  get _email() {
+    return this.email;
   }
 
-  get challenges() {
-    return this.#challenges;
+  get _challenges() {
+    return this.challenges;
   }
-
- constructor(id: Guid, name: string, email: string, challenge: []) {
-   this.#id = id;
-   this.#name = name;
-   this.#email = email;
-   this.#challenges = challenge;
- }
+  
 }
 
