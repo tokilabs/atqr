0ab360ae28e5954d4d509e3ec41c44993d4806bd
Yago Fernandes <yagoalmeida@gmail.com>
2022-01-29 18:39:18 -0300

    Scratch Api

--- apps/atqr/api/src/app/app.controller.ts
+++ apps/atqr/api/src/app/app.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Challenge } from '@atqr/domain';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { CreateChallengeDto } from './dtos/createChallenge.dto';
+import { UpdateCreditCardTokenDto } from './dtos/updateCreditCardToken.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Post('challenge')
+  async createChallenge(
+    @Body() createChallenge: CreateChallengeDto
+  ): Promise<Challenge> {
+    return {} as Challenge; // ME DELETE QUANDO FOR IMPLEMENTAR
+  }
+
+  @Get('challenge/:id')
+  getChallenge(@Param('id') id: string): Challenge {
+    return {} as Challenge; // ME DELETE QUANDO FOR IMPLEMENTAR
+  }
+
+  @Patch('challenge/:id')
+  async updateChallenge(
+    @Param('id') id: string,
+    @Body() updateCreditCardTokenDto: UpdateCreditCardTokenDto
+  ): Promise<Challenge> {
+    return {} as Challenge; // ME DELETE QUANDO FOR IMPLEMENTAR
   }
 }
--- libs/atqr/domain/src/index.ts
+++ libs/atqr/domain/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/atqr-domain';
+export * from './lib/player';
+export * from './lib/challenge-entity';
+export * from './lib/EmailService';
+export * from './lib/payment-method';
