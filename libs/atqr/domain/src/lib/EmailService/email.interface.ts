30d563cbdfc6fe90f4eb4bf5d7579a94220ff670
Gabriela Sanches <gabi@toki.life>
2022-01-29 20:05:32 -0300

    Done with EmailService

--- libs/atqr/domain/src/lib/EmailService/email.interface.ts
+++ libs/atqr/domain/src/lib/EmailService/email.interface.ts
@@ -1,6 +1,24 @@
+import { isEmail } from 'class-validator';
+import * as EmailValidator from 'email-validator';
+import { EmailService } from './email-service';
+
 export interface Email {
   // from: string;
-  to: string;
+  to: EmailAddress;
   subject: string;
   message: string;
+}
+
+export class EmailAddress {
+  email: string;
+
+  constructor(emailText: string) {
+    const isValidEmail = EmailValidator.validate(emailText);
+
+    if (isValidEmail) {
+      this.email = emailText;
+    } else {
+      throw new Error('Invalid Email');
+    }
+  }
 }
