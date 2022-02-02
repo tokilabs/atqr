5faf4ca097ec9d8863644938c9b122b90158a358
Yago Fernandes <yagoalmeida@gmail.com>
2022-02-02 15:32:54 -0300

    Adds player to app module

--- apps/atqr/api/src/app/app.module.ts
+++ apps/atqr/api/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './infra/database/prisma.service';
 import { ChallengeRepository } from './repositories/challenge.repository';
+import { PlayerRepository } from './repositories/player.repository';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService, PrismaService, ChallengeRepository],
+  providers: [AppService, PrismaService, ChallengeRepository, PlayerRepository],
 })
 export class AppModule {}
