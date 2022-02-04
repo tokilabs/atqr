740c4f7c90396140362780dd41c75cc7c7546dee
Gabriela Sanches <gabi@toki.life>
2022-02-04 13:36:52 -0300

    commiit for Yago

--- apps/atqr/api/src/app/app.module.ts
+++ apps/atqr/api/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {}
+}
---
+++ apps/atqr/api/src/app/infra/email/config.module.ts
@@ -0,0 +1,3 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
