225fd11b5c57f44be497405b80c2335a95d4d5a6
Yago Fernandes <yagoalmeida@gmail.com>
2022-02-04 14:34:23 -0300

    Email service complete

--- apps/atqr/api/src/app/infra/email/config.module.ts
+++
@@ -1,3 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-
--- apps/atqr/api/src/app/infra/email/mailgun.service.ts
+++ apps/atqr/api/src/app/infra/email/mailgun.service.ts
@@ -1,4 +1,4 @@
-import { EmailTransporter } from '@atqr/domain';
+import { Email, EmailTransporter } from '@atqr/domain';
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NodeMailgun } from 'ts-mailgun';
@@ -7,53 +7,36 @@ import { NodeMailgun } from 'ts-mailgun';
 export class MailgunService
   implements EmailTransporter, OnApplicationBootstrap
 {
-  apiKey = 'apiKeyToken'; // Set your API key
-  domain = ''; // Set the domain you registered earlier
-  fromEmail = 'noreply@my-sample-app.com'; // Set your from email
-  fromTitle = 'My Sample App'; // Set the name you would like to send from
-  mailer: NodeMailgun;
+  private mailer: NodeMailgun;
+
   constructor(private readonly configService: ConfigService) {}
-  onApplicationBootstrap() {
-    this.apiKey = this.configService.get<string>('MAILGUN_API_KEY');
-    this.domain = this.configService.get<string>('MAILGUN_DOMAIN');
 
-    this.mailer = new NodeMailgun(this.apiKey, this.domain);
+  onApplicationBootstrap() {
+    this.mailer = new NodeMailgun(
+      this.configService.get<string>('MAILGUN_API_KEY'),
+      this.configService.get<string>('MAILGUN_DOMAIN')
+    );
+
+    this.mailer.fromEmail = this.configService.get<string>('MAILGUN_DOMAIN');
+    this.mailer.fromTitle = this.configService.get<string>('MAILGUN_DOMAIN');
+    this.mailer.options = {
+      host: 'api.eu.mailgun.net',
+    };
+
+    this.mailer.init();
   }
-  configs: object;
-  sendMail(): void {
-    this.mailer.send()
-    throw new Error('Method not implemented.');
+
+  async sendMail(email: Email): Promise<void> {
+    try {
+      const result = await this.mailer.send(
+        email.to.email.toString(),
+        email.subject,
+        email.body
+      );
+
+      console.log('Done', result);
+    } catch (error) {
+      console.error('Error: ', error);
+    }
   }
 }
-
-export class Mailer extends NodeMailgun implements OnApplicationBootstrap {
-  apiKey = 'apiKeyToken'; // Set your API key
-  domain = ''; // Set the domain you registered earlier
-  fromEmail = 'noreply@my-sample-app.com'; // Set your from email
-  fromTitle = 'My Sample App'; // Set the name you would like to send from
-
-  constructor(private readonly configService: ConfigService) {
-    super();
-  }
-  onApplicationBootstrap() {
-    this.apiKey = this.configService.get<string>('MAILGUN_API_KEY');
-  }
-}
-
-let mailer = new Mailer(
-  'MAILGUN_API_KEY',
-  '',
-  'noreply@my-sample-app.com',
-  'My Sample App'
-);
-
-mailer.options = {
-  host: 'api.eu.mailgun.net',
-};
-
-mailer.init();
-
-mailer
-  .send('test@example.com', 'Hello!', '<h1>hsdf</h1>')
-  .then((result) => console.log('Done', result))
-  .catch((error) => console.error('Error: ', error));
