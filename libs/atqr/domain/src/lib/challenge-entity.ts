5cd09e89f72d3811ab141b4896eca363284deffc
Yago Fernandes <yagoalmeida@gmail.com>
2022-04-01 17:37:16 -0300

    WIP: Challenge Entity
    commit 71e56ff5a5c03d841a2e0613b72c803d07d272e8
    Author: Raquel <raquel@toki.life>
    Date:   Thu Mar 24 17:14:13 2022 -0300
    
        creating the tests of the class challenge
    
    commit fda2152864d4eb875a2fbd5870514d4d25eb2f00
    Author: Yago Fernandes <yagoalmeida@gmail.com>
    Date:   Fri Apr 1 16:38:27 2022 -0300
    
        Fixing the duplication of the doc challenge-entity and the class challenge
    
        # Conflicts:
        #	libs/atqr/domain/src/lib/challeng-entity.ts
    
    commit 27398a9f73b6deaaabb42af64ee40506d93090e0
    Author: Raquel <raquel@toki.life>
    Date:   Sat Jan 29 17:07:10 2022 -0300
    
        trying to fix the changePaymentMethod
    
    commit 008041290efb3a40ac74c17ff3a02b26744eedcb
    Author: Raquel <raquel@toki.life>
    Date:   Sat Jan 29 15:51:26 2022 -0300
    
        Simplyfing the constructor and changing the changePaymentMethod
    
        # Conflicts:
        #	package-lock.json
    
    commit 5a433825898ddf8e4b8dfa18c2f9d75516e8828f
    Author: Yago Fernandes <yagoalmeida@gmail.com>
    Date:   Fri Apr 1 16:36:59 2022 -0300
    
        adding the get of the class
    
        # Conflicts:
        #	.vscode/settings.json
        #	libs/atqr/domain/src/lib/challeng-entity.ts
        #	package-lock.json
        #	package.json
        #	yarn.lock
    
    commit c2f7baef119d3605c0c3f5126237d04ab99096c3
    Author: Raquel <raquel@toki.life>
    Date:   Tue Jan 25 09:27:41 2022 -0300
    
        fixing the constructor and the method changePaymentMethod
    
        # Conflicts:
        #	libs/atqr/domain/src/lib/challeng-entity.ts
        #	package-lock.json
        #	package.json
        #	yarn.lock
    
    commit a075b963a547659075b86bc207421253438dd7a9
    Author: Yago Fernandes <yagoalmeida@gmail.com>
    Date:   Fri Apr 1 16:36:41 2022 -0300
    
        Creating the challenge class
    
        # Conflicts:
        #	libs/atqr/domain/src/lib/challeng-entity.ts
        #	package.json
        #	yarn.lock

--- libs/atqr/domain/src/lib/challenge-entity.ts
+++ libs/atqr/domain/src/lib/challenge-entity.ts
@@ -1,73 +1,66 @@
 import { Guid } from '@tokilabs/lang/';
-
 import { PaymentMethodEntity } from './payment-method/payment-method';
 import { Player } from './player';
-
-export enum PriceEnum {
-  firstValue = 25,
-  secondValue = 50,
-  thirdValue = 75,
-  fourthValue = 100,
-  fifthValue = 250,
-  sixthValue = 1000,
-}
+// export enum PriceEnum {
+//   firstValue = 25,
+//   secondValue = 50,
+//   thirdValue = 75,
+//   fourthValue = 100,
+//   fifthValue = 250,
+//   sixthValue = 1000,
+// }
 export class Challenge {
   private _id: Guid;
+  private _price: number;
   constructor(
-    private _goal: string,
-    private _deadline: Date,
-    private _price: PriceEnum,
+    private _goal: string, //numero maximo-min de caracteres?
+    private _deadline: Date, //
     private _supervisorName: string,
     private _supervisorEmail: string,
     private _player: Player,
+    id: Guid,
+    price: number,
     private _paymentMethod?: PaymentMethodEntity,
-
     private _status: string = 'Verifying',
-    private _notifiedSupervisor: boolean = false
+    private _notifiedSupervisor: boolean = false //testar - ver se uso email-service da gabi//
   ) {
     this._id = new Guid();
+    this._id = id;
+    if (price >= 25) {
+     this._price = price } else {
+        throw new Error('Selecione um valor acima de 25 reais')
+      }
   }
-
   get id() {
     return this._id;
   }
-
   get goal() {
     return this._goal;
   }
-
   public get status() {
     return this._status;
   }
-
   get deadline() {
     return this._deadline;
   }
-
   get price() {
-    return this._price;
+    return this._price
   }
-
   get notifiedSupervisor() {
     return this._notifiedSupervisor;
   }
-
   get supervisorName() {
     return this._supervisorName;
   }
-
   get supervisorEmail() {
     return this._supervisorEmail;
   }
-
   get player() {
     return this._player;
   }
-
   get paymentMethod() {
     return this._paymentMethod;
   }
-
   changeSupervisor(newSupervisorName: string, newSupervisorEmail: string) {
     this._supervisorName = newSupervisorName;
     this._supervisorEmail = newSupervisorEmail;
