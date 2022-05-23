ecceda7e3492284e83e27a248ef849812d353c38
Gabriela Sanches <gabi@toki.life>
2022-05-23 15:13:03 -0300

    feat: created challenge-entity.controller and started implementation updated the app.controller

--- apps/atqr/api/src/app/app.controller.ts
+++ apps/atqr/api/src/app/app.controller.ts
@@ -17,15 +17,11 @@ import {
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
-import { Mailer } from './infra/email/mailgun.service';
+import { Mailer } from './infra/email/mailer-sevice';
 import { AppService } from './app.service';
-import { CreateChallengeDto } from './dtos/createChallenge.dto';
-import { UpdateCreditCardTokenDto } from './dtos/updateCreditCardToken.dto';
+
 import { ChallengeRepository } from './repositories/challenge.repository';
 import { PlayerRepository } from './repositories/player.repository';
-import ValidationErrors, {
-  ValidationErrorTypes,
-} from './errors/validationErrors';
 
 @Controller()
 export class AppController {
@@ -35,85 +31,4 @@ export class AppController {
     private readonly playerRepository: PlayerRepository,
     private readonly emailService: Mailer
   ) {}
-
-  @Post('challenge')
-  async createChallenge(
-    @Body() challengeDto: CreateChallengeDto
-  ): Promise<Challenge> {
-    try {
-      let player = this.playerRepository.findByEmail(
-        new EmailAddress(challengeDto.playerEmail)
-      );
-
-      if (!player) {
-        player = new Player(
-          challengeDto.playerName,
-          new EmailAddress(challengeDto.playerEmail)
-        );
-      }
-
-      const challenge = new Challenge(
-        challengeDto.goal,
-        challengeDto.deadline,
-        challengeDto.price,
-        challengeDto.supervisorName,
-        challengeDto.supervisorEmail,
-        player
-      );
-
-      if (challengeDto.creditCardToken) {
-        const paymentMethod = new PaymentMethodEntity(
-          PaymentMethodEnum.creditCard,
-          'pagseguro',
-          challengeDto.creditCardToken
-        );
-
-        // Verificar se o token do cartão tem limite para o desafio.(Faz uma cobrança e estorna)
-
-        challenge.changePaymentMethod(paymentMethod);
-
-        this.challengeRepository.create(challenge);
-
-        const emailAddress = new EmailAddress(challengeDto.supervisorEmail);
-        const email = new ChallengeStarted(player);
-        this.emailService.sendMail(email);
-
-        return challenge;
-      } else {
-        this.challengeRepository.create(challenge);
-
-        const emailAddress = new EmailAddress(challengeDto.playerEmail);
-        const email = new SupConfirmation(player);
-        this.emailService.sendMail(email);
-      }
-    } catch (error) {
-      if (error instanceof ValidationErrors) {
-        switch (error.type) {
-          case ValidationErrorTypes.InvalidValue:
-            throw new HttpException(error, HttpStatus.BAD_REQUEST);
-
-          default:
-            throw new HttpException(
-              { message: "We don't know what happen'd", error },
-              HttpStatus.INTERNAL_SERVER_ERROR
-            );
-        }
-      }
-
-      //Pegar os erros e resolver. (Usar try catch)
-    }
-  }
-
-  @Get('challenge/:id')
-  getChallenge(@Param('id') id: string): Challenge {
-    return {} as Challenge; // ME DELETE QUANDO FOR IMPLEMENTAR
-  }
-
-  @Patch('challenge/:id')
-  async updateChallenge(
-    @Param('id') id: string,
-    @Body() updateCreditCardTokenDto: UpdateCreditCardTokenDto
-  ): Promise<Challenge> {
-    return {} as Challenge; // ME DELETE QUANDO FOR IMPLEMENTAR
-  }
 }
