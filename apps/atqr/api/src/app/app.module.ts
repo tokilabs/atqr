702e4d673150942c879ab3204049269be80b5da0
Yago F <yagoalmeida@gmail.com>
2022-05-26 11:18:06 -0300

    feat(app.module): adds challenge controller to app.module

--- apps/atqr/api/src/app/app.module.ts
+++ apps/atqr/api/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChallengeController } from './challenge-entity.controller';
 import { PrismaService } from './infra/database/prisma.service';
 import { StripeService } from './infra/payment/stripe.service';
 import { ChallengeRepository } from './repositories/challenge.repository';
@@ -9,7 +10,7 @@ import { PlayerRepository } from './repositories/player.repository';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController],
+  controllers: [AppController, ChallengeController],
   providers: [
     AppService,
     PrismaService,
