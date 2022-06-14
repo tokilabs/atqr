8364dc2c52d090d246c301f0c19065717115651a
Alberto nunes <bisneto.alberto@gmail.com>
2022-06-14 15:55:46 -0300

    refactor: js to ts

--- apps/atqr/web/src/pages/lastPlayersSession/getLastPlayers.ts
+++ apps/atqr/web/src/pages/lastPlayersSession/getLastPlayers.ts
@@ -1,7 +1,7 @@
 async function getLastChallenges() {
   try {
     const { data } = await axios("https://jsonplaceholder.typicode.com/posts");
-    let topicContent = document.querySelectorAll(".topic-content");
+    const topicContent = document.querySelectorAll(".topic-content");
     topicContent.forEach((value, index) => {
       value.querySelector("h3").textContent = data[index].title;
     });
