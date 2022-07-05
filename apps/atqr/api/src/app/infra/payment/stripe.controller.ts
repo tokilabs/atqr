1eecd0dd1c60a3394e224710fe027af0c302895a
Yago Fernandes <yagoalmeida@gmail.com>
2022-07-05 20:29:47 -0300

    chore(stripe.controller): cleanup. Removes unnecessary imports and formating

--- apps/atqr/api/src/app/infra/payment/stripe.controller.ts
+++ apps/atqr/api/src/app/infra/payment/stripe.controller.ts
@@ -1,13 +1,9 @@
 import { Controller, Get, Post } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { StripeService } from './stripe.service';
 
 @Controller('stripe')
 export class StripeController {
-  constructor(
-    private readonly stripeService: StripeService,
-    private readonly configService: ConfigService
-  ) {}
+  constructor(private readonly stripeService: StripeService) {}
 
   @Get('/stripe-key')
   getPublicKey(): string {
