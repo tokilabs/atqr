42f90181ef4c69a3aa6fdc34a71509836eb63505
Yago Fernandes <yagoalmeida@gmail.com>
2022-07-27 12:58:44 -0300

    feat: imported ChallengeController To the controllers prop on appModule

--- apps/atqr/api/src/app/app.module.ts
+++ apps/atqr/api/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { DeadlineMonitorService, NotificationService } from '@atqr/domain';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ChallengeController } from './challenge.controller';
-import { Mailer, PrismaService, StripeModule, StripeService } from './infra';
+import { Mailer, PrismaService, StripeService } from './infra';
 import { ChallengeRepository, PlayerRepository } from './repositories';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), StripeModule],
-  controllers: [ChallengeController],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [ ChallengeController],
   providers: [
     PrismaService,
     ChallengeRepository,
