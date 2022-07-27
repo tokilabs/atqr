5e55dcbd26d8b1e14fdfc5bc301fe1e4d215ad32
Yago Fernandes <yagoalmeida@gmail.com>
2022-07-27 10:02:38 -0300

    feat: Added a player propriety to deal with mistakes in challenge repository

--- apps/atqr/api/src/app/infra/database/prisma.service.ts
+++ apps/atqr/api/src/app/infra/database/prisma.service.ts
@@ -1,8 +1,10 @@
+import { Player } from '@atqr/domain';
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  player: Player;
   async onModuleInit() {
     await this.$connect();
   }
