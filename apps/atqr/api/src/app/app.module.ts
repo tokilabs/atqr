40937cd9d167e2b7103f3962a74358affa7decf3
Alberto Nunes <bisneto.alberto@gmail.com>
2022-08-25 15:48:22 -0300

    feature: i did Filter catch all the HttpExceptions and make a response

--- apps/atqr/api/src/app/app.module.ts
+++ apps/atqr/api/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { ChallengeController } from './challenge.controller';
-import { HttpExceptionFilter } from './filter/httpExceptions.filter';
+import { HttpExceptionFilter } from './filter/httpExceptions';
 import { Mailer, PrismaService, StripeService } from './infra';
 import { ChallengeRepository, PlayerRepository } from './repositories';
 
--- apps/atqr/api/src/app/filter/httpExceptions.ts
+++ apps/atqr/api/src/app/filter/httpExceptions.ts
@@ -11,18 +11,16 @@ export class HttpExceptionFilter implements ExceptionFilter {
   constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
 
   catch(exception: HttpException, host: ArgumentsHost): void {
-
     const { httpAdapter } = this.httpAdapterHost;
     const ctx = host.switchToHttp();
 
-    const httpStatus = exception.getStatus()
-    const message = exception.message
-    const path = httpAdapter.getRequestUrl(ctx.getRequest())
-
+    const httpStatus = exception.getStatus();
+    const message = exception.message;
+    const path = httpAdapter.getRequestUrl(ctx.getRequest());
     const responseBody = {
       statusCode: httpStatus,
       message: message,
-      path: path ,
+      path: path,
     };
 
     httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
