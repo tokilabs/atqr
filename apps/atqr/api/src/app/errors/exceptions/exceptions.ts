f579d08827948736f8ffa1bc4c26bcdd2f9e655d
Alberto Nunes <bisneto.alberto@gmail.com>
2022-08-26 11:07:01 -0300

    refactor: the code don´t need an external function.

--- apps/atqr/api/src/app/errors/exceptions/exceptions.ts
+++ apps/atqr/api/src/app/errors/exceptions/exceptions.ts
@@ -1,15 +1,11 @@
-import { ThrowHttpExceptions } from './throwHttpExceptions';
-import {
-  CommonErrorTypes,
-  ErrorTypes,
-  ValidationErrorTypes,
-} from '../validationError';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 export interface IExceptions {
+  error: Error;
   messageError: string;
   messageFixError: string;
-  error: Error;
-  type: ErrorTypes;
+
+  httpExceptions(error: Error): void;
 }
 
 export class Exceptions {
@@ -18,90 +14,113 @@ export class Exceptions {
     this.exception = exception;
   }
   throwException(): void {
-    this.exception.error.message =
+    const message: string =
       this.exception.messageError + ' ' + this.exception.messageFixError;
-
-    return ThrowHttpExceptions(this.exception);
+    this.exception.error.message = message;
+    return this.exception.httpExceptions(this.exception.error);
   }
 }
 
 export class BadRequest implements IExceptions {
-  public type: ErrorTypes = ValidationErrorTypes.InvalidValue;
   constructor(
+    public error: Error,
     public messageError: string,
-    public messageFixError: string,
-    public error: Error
+    public messageFixError: string
   ) {}
+
+  httpExceptions(error: Error): void {
+    throw new HttpException(error, HttpStatus.BAD_REQUEST);
+  }
 }
 
 export class NotFound implements IExceptions {
-  public type: ErrorTypes = CommonErrorTypes.NotFound;
   constructor(
+    public error: Error,
     public messageError: string,
-    public messageFixError: string,
-    public error: Error
+    public messageFixError: string
   ) {}
+
+  httpExceptions(error: Error): void {
+    throw new HttpException(error, HttpStatus.NOT_FOUND);
+  }
 }
 export class MethodNotAllowed implements IExceptions {
-  public type: ErrorTypes = CommonErrorTypes.MethodNotAllowed;
-
   constructor(
+    public error: Error,
     public messageError: string,
-    public messageFixError: string,
-    public error: Error
+    public messageFixError: string
   ) {}
+
+  httpExceptions(error: Error): void {
+    throw new HttpException(error, HttpStatus.METHOD_NOT_ALLOWED);
+  }
 }
 export class Conflict implements IExceptions {
-  public type: ErrorTypes = CommonErrorTypes.Conflict;
-
   constructor(
+    public error: Error,
     public messageError: string,
-    public messageFixError: string,
-    public error: Error
+    public messageFixError: string
   ) {}
+
+  httpExceptions(error: Error): void {
+    throw new HttpException(error, HttpStatus.CONFLICT);
+  }
 }
 
 export class UnprocessableEntity implements IExceptions {
-  public type: ErrorTypes = CommonErrorTypes.UnprocessableEntity;
-
   constructor(
+    public error: Error,
     public messageError: string,
-    public messageFixError: string,
-    public error: Error
+    public messageFixError: string
   ) {}
+
+  httpExceptions(error: Error): void {
+    throw new HttpException(error, HttpStatus.UNPROCESSABLE_ENTITY);
+  }
 }
 
 export class NotImplemented implements IExceptions {
-  public type: ErrorTypes = CommonErrorTypes.NotImplemented;
-
   constructor(
+    public error: Error,
     public messageError: string,
-    public messageFixError: string,
-    public error: Error
+    public messageFixError: string
   ) {}
+
+  httpExceptions(error: Error): void {
+    throw new HttpException(error, HttpStatus.NOT_IMPLEMENTED);
+  }
 }
 export class BadGateway implements IExceptions {
-  public type: ErrorTypes = CommonErrorTypes.BadGateway;
   constructor(
+    public error: Error,
     public messageError: string,
-    public messageFixError: string,
-    public error: Error
+    public messageFixError: string
   ) {}
+
+  httpExceptions(error: Error): void {
+    throw new HttpException(error, HttpStatus.BAD_GATEWAY);
+  }
 }
 
 export class ServiceUnavailable implements IExceptions {
-  public type: ErrorTypes = CommonErrorTypes.ServiceUnavailable;
   constructor(
+    public error: Error,
     public messageError: string,
-    public messageFixError: string,
-    public error: Error
+    public messageFixError: string
   ) {}
+
+  httpExceptions(error: Error): void {
+    throw new HttpException(error, HttpStatus.SERVICE_UNAVAILABLE);
+  }
 }
 export class GatewayTimeout implements IExceptions {
-  public type: ErrorTypes = CommonErrorTypes.GatewayTimeout;
   constructor(
+    public error: Error,
     public messageError: string,
-    public messageFixError: string,
-    public error: Error
+    public messageFixError: string
   ) {}
+
+  httpExceptions(error: Error): void {
+    throw new HttpException(error, HttpStatus.GATEWAY_TIMEOUT);
+  }
 }
--- apps/atqr/api/src/app/errors/index.ts
+++ apps/atqr/api/src/app/errors/index.ts
@@ -1,4 +1,3 @@
 export * from './exceptions/exceptions'
-export * from './exceptions/throwHttpExceptions'
 export * from './mailgunError';
 export * from './validationError';
