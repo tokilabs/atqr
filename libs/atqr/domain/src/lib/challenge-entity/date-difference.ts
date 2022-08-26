d4a9944a63709d4bef59a1320f3c84584fe5c8b6
Gabriela Sanches <gabi@toki.life>
2022-08-26 17:16:28 -0300

    feat: started treating error on dateDiff()

---
+++ libs/atqr/domain/src/lib/challenge-entity/date-difference.ts
@@ -0,0 +1,18 @@
+import { Exception } from '@tokilabs/lang';
+
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+// a and b are javascript Date objects
+export function dateDiff(a, b) {
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  try {
+    dateDiff(this.a, this.b);
+  } catch {
+    Exception;
+    throw { Exception };
+  }
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
