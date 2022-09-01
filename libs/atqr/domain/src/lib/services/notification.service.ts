326b365059746ceb7cde5394bdc29d8e263e0ea9
Alberto Nunes <bisneto.alberto@gmail.com>
2022-09-01 15:55:52 -0300

    feature: I used the notification service as a base to notify the player

--- libs/atqr/domain/src/lib/services/notification.service.ts
+++ libs/atqr/domain/src/lib/services/notification.service.ts
@@ -1,8 +1,7 @@
 import { Exception } from '@tokilabs/lang';
-import { ChallengeStatus } from '../challenge';
-import { Email ,DeadLineEmail, IMailer } from '../EmailService';
-import { IChallengeRepository } from '../challenge/challengeRepo.interface';
-
+import { Challenge, ChallengeStatus } from '../challenge';
+import { Congrats, Email, IMailer, PayThePrice } from '../EmailService';
+import { IChallengeRepository } from '../repository.interfaces';
 
 export class NotificationService {
   constructor(
@@ -17,11 +16,13 @@ export class NotificationService {
           if (c.updateOverdueStatus() === true) {
             try {
               const player = c.player;
-              const email = new DeadLineEmail(
+              const email = new Email(
                 player,
+                'OverdueChallenge',
+                'Your time is over'
               );
               this.mailer.sendMail(email);
-              c.updateOverdueStatus();
+              this.challengeRepository.update(c);
             } catch (err) {
               throw new Exception(
                 `Error updating overdue status of Challenge ${c.id}: ${
@@ -33,5 +34,16 @@ export class NotificationService {
         })
       );
     });
+  }
+  public notifyCompletedChallenges(challenge: Challenge) {
+    if (challenge.status == ChallengeStatus.Completed) {
+      const email = new Congrats(challenge.player);
+      this.mailer.sendMail(email);
+    } else {
+      const email = new PayThePrice(challenge.player);
+      this.mailer.sendMail(email);
+      return true;
+    }
+    this.challengeRepository.update(challenge);
   }
 }
