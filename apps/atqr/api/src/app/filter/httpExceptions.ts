50de957c2974353d6b5180d7d974eecba3fd7dac
Alberto Nunes <bisneto.alberto@gmail.com>
2022-09-02 20:16:59 -0300

    refactor: remade the filter to take only treated error

--- apps/atqr/api/src/app/filter/httpExceptions.ts
+++ apps/atqr/api/src/app/filter/httpExceptions.ts
@@ -3,17 +3,34 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
 
-@Catch(HttpException)
+@Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
   constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
 
-  catch(exception: HttpException, host: ArgumentsHost): void {
+  catch(exception: HttpException, host: ArgumentsHost): string | void {
+    if (!(exception instanceof HttpException)) {
+      console.log('You need handle this error');
+      console.log(
+        "Error handling example: throw new NotFoundException('we didn't find what you were looking for') "
+      );
+      console.log(exception);
+      exception = new InternalServerErrorException(
+        "We don't know what happen'd"
+      );
+    }
+    if (exception.message.length <= 10) {
+      console.log('Your message is too short. Do a better one');
+      exception = new InternalServerErrorException(
+        "We don't know what happen'd"
+      );
+    }
+
     const { httpAdapter } = this.httpAdapterHost;
     const ctx = host.switchToHttp();
-
     const httpStatus = exception.getStatus();
     const message = exception.message;
     const path = httpAdapter.getRequestUrl(ctx.getRequest());
