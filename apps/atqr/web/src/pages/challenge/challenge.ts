23f11d31e4d818a477bd935c20090c78e9e92ed6
Gabriela Sanches <gabi@toki.life>
2022-09-12 16:44:29 -0300

    refactor: refactored create challenge task(challenge yourself 'n' challenge someone) consuming api

--- apps/atqr/web/src/pages/challenge/challenge.ts
+++ apps/atqr/web/src/pages/challenge/challenge.ts
@@ -30,23 +30,7 @@ export const challengeDto: CreateChallengeDto = {
   supervisorEmail,
 };
 
-const createChallenge = await atqrApi.challenges.create(challengeDto);
-const getChallenge = async () => {
-  try {
-    const res = await axiosInstance({
-      method: 'get',
-      url: '/:id',
-    });
-    return res.data;
-  } catch (err) {
-    if (err instanceof AxiosError) {
-      console.log('error message: ');
-      return Error;
-    }
-  }
-};
-
-export const challenge = await getChallenge();
+const challenge = await atqrApi.challenges.create(challengeDto);
 
 //page construction
 const nextStepBtn = document.getElementById('proximo-passo-btn');
@@ -54,12 +38,7 @@ nextStepBtn.addEventListener('click', challenge);
 const goalTitle = document.getElementById('main-title');
 goalTitle.innerHTML = 'seu desafio ' + challenge.goal + ' foi criado';
 
-const getDeadline = challenge.deadline;
-const setDeadline = document.getElementById('deadline');
-setDeadline.setAttribute('data-countdown', getDeadline);
-const setDeadline2 = document.getElementById('deadline2');
-setDeadline2.innerHTML = getDeadline;
-
+//page challenge created
 const resumeCreatedChallenge = document.getElementById('text');
 resumeCreatedChallenge.innerHTML =
   'seu desafio é' +
@@ -69,3 +48,5 @@ resumeCreatedChallenge.innerHTML =
   '. Caso não cumpra com seu objetivo, será cobrado de você' +
   challenge.price +
   'reais';
+
+// end page challenge created
