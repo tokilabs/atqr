8d01dc245779f09e850f8626aa332a218f3218ae
Raquel <raquel@toki.life>
2022-10-14 03:09:06 -0300

    perf(emails templates): did a base template for all emails

--- libs/atqr/domain/src/lib/EmailService/templates/supConfirmation.ts
+++ libs/atqr/domain/src/lib/EmailService/templates/supConfirmation.ts
@@ -1,14 +1,49 @@
 import { Player } from '../../player/player.entity';
 import { Email } from '../email.service';
+import axios, { AxiosError } from 'axios';
+import * as pug from 'pug';
+import * as fs from 'fs';
+import path = require('path');
 
+const pugFile = pug.compileFile(path.join( __dirname, 'SupConfirmation.pug'))
 
 
 export class SupConfirmation extends Email {
   constructor(to: Player) {
     super(
       to,
-      'Você foi convidado a ser supervisor de ${playerName}...',
-      'Explicação do jogo'
+      'Você foi convidado a ser supervisor de ${playerName}...', pugFile({
+        player: to,
+        supervisor: 
+      })
     );
   }
 }
+
+const axiosInstance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com/',
+});
+
+
+
+const getPlayer = async () => {
+  try {
+    const res = await axiosInstance({
+      method: 'get',
+      url: 'challenge/player',
+      data: 'player'
+    });
+    return res.data;
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      console.log('error message: ');
+      return '';
+    } else {
+      console.log('unexpected error: ', Error);
+      return 'An unexpected error occurred';
+    }
+  }
+};
+
+let playerName = document.getElementsByTagName('')
+
