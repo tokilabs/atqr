67a6d6da3915253a1278b103306f60857274243d
Raiane <raiane.rze@gmail.com>
2022-10-14 13:07:38 -0300

    import pug in ts file and compile

--- libs/atqr/domain/src/lib/EmailService/templates/challengeStarted.ts
+++ libs/atqr/domain/src/lib/EmailService/templates/challengeStarted.ts
@@ -1,8 +1,20 @@
 import { Player } from '../../player/player.entity';
 import { Email } from '../email.service';
+import * as pug from 'pug';
+import path = require('path');
+
+const compileTemplate = pug.compileFile(
+  path.join(__dirname, 'challengeStarted.pug')
+);
 
 export class ChallengeStarted extends Email {
   constructor(to: Player) {
-    super(to, 'Seu desafio começou', 'Mensagem de boas vindas');
+    super(
+      to,
+      'Seu desafio começou',
+      compileTemplate({
+        player: to,
+      })
+    );
   }
 }
