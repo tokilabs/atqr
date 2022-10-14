3861338d86150ad19d024a8748b49aaff3965f5a
Raiane <raiane.rze@gmail.com>
2022-10-14 19:39:51 -0300

    deadlineEmail template update

--- libs/atqr/domain/src/lib/EmailService/templates/deadLineEmail.ts
+++ libs/atqr/domain/src/lib/EmailService/templates/deadLineEmail.ts
@@ -1,10 +1,17 @@
 import { Player } from '../../player/player.entity';
 import { Email } from '../email.service';
+import * as pug from 'pug';
+import path = require('path');
 
+const compileTemplate = pug.compileFile(
+  path.join(__dirname, 'deadLineEmail.pug')
+);
 
 export class DeadLineEmail extends Email {
   constructor(to: Player) {
-    super(to, 'E aí?', ' .... ');
+    super(to, 'E aí?', ' .... ', compileTemplate({
+      player: to,
+    }));
   }
 }
 
