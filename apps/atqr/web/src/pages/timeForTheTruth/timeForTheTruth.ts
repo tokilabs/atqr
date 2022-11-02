8206b008eb6002045f5703086ec740549986af35
Gabriela Sanches <gabi@toki.life>
2022-11-02 13:15:24 -0300

    feat: updated imports from Meeta and finished logic to change challenge status

--- apps/atqr/web/src/pages/timeForTheTruth/timeForTheTruth.ts
+++ apps/atqr/web/src/pages/timeForTheTruth/timeForTheTruth.ts
@@ -1,31 +1,37 @@
-import { ChallengeStatus } from "@atqr/domain";
-import {ChallengeController} from "../../../../api/src/app/challenge.controller";
-import { challenge } from "../challenge/challenge";
-
+import { ChallengeStatus } from '@atqr/domain';
+import { ChallengeController } from '../../../../api/src/app/challenge.controller';
+import { challenge } from '../challenge/challenge';
 
 export class TimeForTheTruth {
+  constructor(private challengeController: ChallengeController) {}
 
-    constructor( private  challengeController: ChallengeController)
-    {}
-     
-    challenge = challenge
-    
-         congratsButton (){
-           let challengeUpdated = this.challengeController.updateStatus(challenge, ChallengeStatus.Completed)
-           return challengeUpdated
-          }
-        
-         
-      
-           reproveButton (){
-            let challengeUpdated = this.challengeController.updateStatus(challenge, ChallengeStatus.Failed)
-            return challengeUpdated
-         }
+  challenge = challenge;
+
+  congratsButton() {
+    let challengeStatusUpdated = this.challengeController.updateStatus(
+      challenge,
+      ChallengeStatus.Completed
+    );
+    return challengeStatusUpdated;
+  }
+
+  reproveButton() {
+    let challengeStatusUpdated = this.challengeController.updateStatus(
+      challenge,
+      ChallengeStatus.Failed
+    );
+    return challengeStatusUpdated;
+  }
 }
+
+
+const player = document.getElementById('player');
+const resumeChallenge = document.getElementById('description');
+const deadline = document.getElementById('deadline');
+const text = document.getElementById('text')
 
 const congratsButton = document.getElementsByClassName('congratsButton');
 const reproveButton = document.getElementsByClassName('reproveButton');
 
-
-
-  
+resumeChallenge.innerHTML = 'O desafio é' + challenge.description;
+deadline.innerHTML = 'Até: ' + deadline;
