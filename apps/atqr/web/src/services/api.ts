ca1d2142afaa5bb1a8095d5a4a7915547c5e577f
Alberto Nunes <bisneto.alberto@gmail.com>
2022-11-14 15:03:11 -0300

    Alberto/atqr 92: Refactor Update challenge methods (#31)
    * feature: create a global dto to endpoint patch
    
    * implement the methods in endpoint
    
    * the methods in front are updated
    
    * refactor: Passed the logic to a middleware
    
    * formatted document
    
    * feat(controller): added a route for each method in controller
    
    added a different patch route for method in controller
    
    * refactor(atqr api): adds api indexes and absolutes paths to api and Tsconfig
    
    * refactor: i removed useless imports and cleaned the code
    
    * fix(api/dto): renames Update Challenge Dto
    
    * chore(api): fixes spelling mistakes and other minor fixes
    
    Co-authored-by: Yago Fernandes <yagoalmeida@gmail.com>

---
+++ apps/atqr/web/src/services/api.ts
@@ -0,0 +1,98 @@
+import { Challenge, ChallengeStatus } from '@atqr/domain';
+import { Guid } from '@tokilabs/lang';
+import axios from 'axios';
+
+import { CreateChallengeDto, UpdateCreditCardTokenDto } from '@atqr/api';
+
+const baseUrl = 'http://localhost:3333/api/';
+
+const api = axios.create({
+  baseURL: baseUrl,
+  timeout: 1000,
+});
+
+export const atqrApi = {
+  challenges: {
+    create: async (createChallengeDto: CreateChallengeDto) => {
+      try {
+        const created = await api.post(
+          `challenge/challenge`,
+          createChallengeDto
+        );
+        return created;
+      } catch (error) {
+        console.log(error);
+        return error;
+      }
+    },
+    getLatest: async (amount: number) => {
+      try {
+        const latest = await api.get(`challenge/last-challenges/${amount}`);
+        return latest;
+      } catch (error) {
+        console.log(error);
+        return error;
+      }
+    },
+    challengeUpdated: {
+      statusUpdated: async (id: Guid, updateStatus: ChallengeStatus) => {
+        try {
+          const statsUpdated = await api.patch(
+            `/challenge/${id}`,
+            updateStatus
+          );
+          return statsUpdated;
+        } catch (error) {
+          console.log(error);
+          return error;
+        }
+      },
+      //TODO: need implement in controller
+      paymentUpdated: async (
+        id: Guid,
+        updateCreditCardTokenDto: UpdateCreditCardTokenDto
+      ) => {
+        try {
+          const paymentUpdated = await api.patch(
+            `/challenge/${id}`,
+            updateCreditCardTokenDto
+          );
+          return paymentUpdated;
+        } catch (error) {
+          console.log(error);
+          return error;
+        }
+      }, //TODO: need implement in controller
+      changedSupervisor: async (id: string) => {
+        try {
+          const paymentUpdated = await api.patch(`/challenge/${id}`);
+          return paymentUpdated;
+        } catch (error) {
+          console.log(error);
+          return error;
+        }
+      },
+    },
+  },
+
+  payment: {
+    publicKey: async () => {
+      try {
+        const key = await api.get(`stripe/public-key`);
+        return key;
+      } catch (error) {
+        console.log(error);
+        return error;
+      }
+    },
+    setupIntent: async () => {
+      try {
+        const intent = await api.post(`stripe/setup-intent`);
+        return intent;
+      } catch (error) {
+        console.log(error);
+        return error;
+      }
+    },
+  },
+};
