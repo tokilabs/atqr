247cb5a415e37e09195f5401cd17c423680eb62b
Angelica <angelica@toki.life>
2022-12-06 11:50:53 -0300

    feat(web): i created newSupervisor.pug and newsupervisor.ts
    I add newSupervisor.pug and newSupwevisor.ts.

---
+++ apps/atqr/web/src/pages/newSupervisor/newSupervisor.ts
@@ -0,0 +1,14 @@
+
+const urlParams = new URLSearchParams(window.location.search);
+
+const url = urlParams.get("id");
+
+console.log(url);
+
+/* const nameSupervisor = document.getElementById('#nameSupervisor')
+
+const emailSupervisor = document.getElementById('#emailSupervisor')
+
+console.log(nameSupervisor)
+
+ */
