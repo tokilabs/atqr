af1d088ec2f3332cde9e621e71e233246166e4ec
Raquel <raquel@toki.life>
2023-01-09 16:22:03 -0300

    refactor(enums defined in the disl file): adding the enums needed defined in the disl definition

--- apps/atqr/web/src/services/interfaces.ts
+++ apps/atqr/web/src/services/interfaces.ts
@@ -1,65 +1,85 @@
-export enum SupervisorEnum {
-    'notInvited',
-    'invited',
-    'accepted',
-    'askedIfTheGoalIsAccomplished',
-    'repliedIfTheGoalWasAccomplished',
-  }
+export enum ChallengeStatus {
+  Unverified = 'Unverified',
+  Ongoing = 'Ongoing',
+  Overdue = 'Overdue',
+  Abandoned = 'Abandoned',
+  Canceled = 'Canceled',
+  Finished = 'Finished',
+}
 
-  export enum PaymentMethodEnum {
-    'creditCard' = 'creditCard',
-    'debitCard' = 'debitCard',
-  }
+export enum ContenderOutcome {
+  StillPlaying = 'StillPlaying',
+  Succeeded = 'Succeeded',
+  Failed = 'Failed',
+}
 
-  export enum ChallengeStatus {
-    Ongoing = 'Ongoing',
-    Completed = 'Completed',
-    Failed = 'Failed',
-    Overdue = 'Overdue',
-    Created = 'Created'
-  }
+export enum ParticipationStatus {
+  NotRequested = 'NotRequested',
+  Requested = 'Requested',
+  Accepted = 'Accepted',
+  Rejected = 'Rejected',
+  Ignored = 'Ignored',
+  CanceledByContender = 'CanceledByContender',
+}
 
-  export const updateStatus = (id: string, status: ChallengeStatus) => {
-    if(status == ChallengeStatus.Overdue){
-      return false
-    }else{
-      return status
-    }
+export enum JudgementStatus {
+  NotDueYet = 'NotDueYet',
+  NotRequested = 'NotRequested',
+  Requested = 'Requested',
+  Ignored = 'Ignored',
+  Judged = 'Judged',
+}
 
-  }
-  export interface IChallenge {
-    id: string;
-    price: number;
-    deadline: Date;
-    goal: string;
-    status?: ChallengeStatus;
-    supervisorName: string;
-    supervisorEmail: string;
-    player: IPlayer;
-    playerName: string
-    supervisorStatus: SupervisorEnum;
-    updateStatus: ChallengeStatus
-  }
+export enum ParticipationRole {
+  Contender = 'Contender',
+  Judge = 'Judge',
+}
 
-  export interface IEmailAddress {
-    value: string;
-  }
+export enum PaymentMethodEnum {
+  'creditCard' = 'creditCard',
+  'debitCard' = 'debitCard',
+}
 
-  export interface IPlayer {
-    id: string;
-    email: typeEmailAddress;
-    challenges: playerChallenges;
-    name: string;
+export const updateStatus = (id: string, status: ChallengeStatus) => {
+  if (status == ChallengeStatus.Overdue) {
+    return false;
+  } else {
+    return status;
   }
+};
+export interface IChallenge {
+  id: string;
+  price: number;
+  deadline: Date;
+  goal: string;
+  status?: ChallengeStatus;
+  supervisorName: string;
+  supervisorEmail: string;
+  player: IPlayer;
+  playerName: string;
+  judgmentStatus: JudgementStatus;
+  updateStatus: ChallengeStatus;
+}
 
-  export interface PaymentMethodEntity {
-    id: string;
-    method: PaymentMethodEnum;
-    paymentService: `pagar.me` | `pagseguro`;
-    token: string;
-  }
+export interface IEmailAddress {
+  value: string;
+}
 
-  export interface CreateChallengeDto {
+export interface IPlayer {
+  id: string;
+  email: typeEmailAddress;
+  challenges: playerChallenges;
+  name: string;
+}
+
+export interface PaymentMethodEntity {
+  id: string;
+  method: PaymentMethodEnum;
+  paymentService: `pagar.me` | `pagseguro`;
+  token: string;
+}
+
+export interface CreateChallengeDto {
   goal: string;
   deadline: Date;
   price: number;
@@ -72,18 +92,18 @@ export enum SupervisorEnum {
     paymentService: `pagar.me` | `pagseguro`;
     token: string;
   };
-  }
+}
 
-  export interface UpdateCreditCardTokenDto {
-    challengeId: string;
-    // TODO: @yfernandes Validate token type
-    creditCardToken: string;
-  }
+export interface UpdateCreditCardTokenDto {
+  challengeId: string;
+  // TODO: @yfernandes Validate token type
+  creditCardToken: string;
+}
 
-  export type typeEmailAddress = IEmailAddress;
-  export type typePlayer = IPlayer;
-  export type playerChallenges = IChallenge[];
-  export type challenge = IChallenge;
+export type typeEmailAddress = IEmailAddress;
+export type typePlayer = IPlayer;
+export type playerChallenges = IChallenge[];
+export type challenge = IChallenge;
 
-  export type playerName = '';
-  export type paymentMethod = PaymentMethodEntity;
+export type playerName = '';
+export type paymentMethod = PaymentMethodEntity;
