1b07fec7c1096c312a46edf7837a63ff653a4a97
Gabriela Sanches <gabi@toki.life>
2023-01-09 18:46:41 -0300

    perf(contacts.disl and email.interface.ts): emailAddress inteface, OV Email on disl and domain
    interface for EmailAddress now exists and has validation test. Concept EmailAddress is in
    contacts.disl

--- libs/atqr/domain/src/lib/EmailService/email.interface.ts
+++ libs/atqr/domain/src/lib/EmailService/email.interface.ts
@@ -1,19 +1,37 @@
-import { EmailAddress } from './emailAddress';
+import { RegEx } from 'regex';
+
 export interface IEmail {
   to: EmailAddress;
   subject: string;
   message?: string;
 }
 
+function validateEmailAddress(emailAddress: string): EmailAddress {
+  const regex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+  const emails = emailAddress[''];
+  const testEmails = emails.forEach((emailAddress) => {
+    regex.test(emailAddress);
+    console.log(regex.test(emailAddress));
+  });
+  console.log(testEmails);
+  return testEmails;
+}
+
+validateEmailAddress('gabi@toki.life');
+export interface EmailAddress {
+  emailAddress: string;
+  validateEmailAddress(): typeof validateEmailAddress;
+}
+
 export class Email implements IEmail {
   constructor(
     public to: EmailAddress,
     public subject: string,
-    public message?: string
+    public body: string,
   ) {
     this.to = to;
     this.subject = subject;
-    this.message = message;
+    this.body = body;
   }
 
   public get email() {
