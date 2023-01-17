b785a91b155970fdab1086328ef05a8d36f2e876
Raquel <raquel@toki.life>
2023-01-17 15:22:08 -0300

    (ATQR-123) Updates enums to match concept set (#76)
    * chore(enums from the concept-set disl): updated the enums to match the definition in disl
    
    * fix(domain/challenge): removes Duplicate enums from domain challenge
    
    * refactor(domain): moves misplaced concept-set enums from Web app to Domain
    
    * refactor(web/services): removes duplicated enums and updates indexes
    
    * fix(challenge.entity): changes supervisor status to participation status
    
    * fix(web/service/interface): removes payment enum from interfaces
    
    Co-authored-by: Yago Fernandes <yagoalmeida@gmail.com>

--- libs/atqr/domain/src/index.ts
+++ libs/atqr/domain/src/index.ts
@@ -1,5 +1,1 @@
-export * from './lib/challenge/';
-export * from './lib/EmailService';
-export * from './lib/PaymentMethod';
-export * from './lib/player/';
-export * from './lib/services';
+export * from './lib';
