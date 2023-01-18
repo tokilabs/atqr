cf71ab3a96a47524e0877ede2da5674dc458c344
Raiane Rezende <90356301+rairezende@users.noreply.github.com>
2023-01-18 18:33:29 -0300

    (ATQR-124) Implemented Enrollment and Officiation Object Values (#75)
    * feat: 🎸 implemented Enrollment and Officiation Object Values
    
    ✅ Closes: #124
    
    * fix(domain/player): creates stubs for player's value objects
    
    Co-authored-by: Yago Fernandes <yagoalmeida@gmail.com>

---
+++ libs/atqr/domain/src/lib/players/Officiation.ts
@@ -0,0 +1,39 @@
+import { ValueObject } from '../../utils/valueObject';
+import { Challenge } from '../challenge';
+import { JudgementStatus, User } from '../types';
+import { Enrollment } from './Enrollment';
+
+export class Officiation extends ValueObject<Officiation> {
+  constructor(
+    public readonly challenge: Challenge,
+    public readonly judge: User,
+    public readonly contenders: Enrollment[],
+    public readonly judgementStatus: JudgementStatus
+  ) {
+    super(Officiation, ['contenders', 'judge', 'challenge', 'judgementStatus']);
+  }
+
+  public setChallenge(value: Challenge) {
+    return this.newInstanceWith({
+      challenge: value,
+    });
+  }
+
+  public setJudge(value: User) {
+    return this.newInstanceWith({
+      judge: value,
+    });
+  }
+
+  public setContenders(value: Enrollment[]) {
+    return this.newInstanceWith({
+      contenders: value,
+    });
+  }
+
+  public setJudgementStatus(value: JudgementStatus) {
+    return this.newInstanceWith({
+      judgementStatus: value,
+    });
+  }
+}
---
+++ libs/atqr/domain/src/lib/types/stubs.ts
@@ -0,0 +1,12 @@
+/*
+   ---- HEY THIS ENTIRE FILE IS JUST ONE BIG HACK ----
+  IT'S ENTIRE PURPOSE IS TO BE DELETED AS QUICKLY AS POSSIBLE
+  DO NOT LET IT LIVE
+
+  As soon as everything in here has been created for real delete it
+  @yfernandes
+*/
+
+export type User = Record<string, unknown>;
+export type PaymentMethod = Record<string, unknown>;
+export type JudgementStatus = Record<string, unknown>;
