7e8c8a9bcfc8866beec69a1df5cd53ff9094a2c8
Alberto Nunes <bisneto.alberto@gmail.com>
2023-01-18 16:22:04 -0300

    test: tests have been improved

--- libs/atqr/domain/src/lib/EmailService/email.service.test.ts
+++ libs/atqr/domain/src/lib/EmailService/email.service.test.ts
@@ -2,13 +2,17 @@ import { EmailService } from './email.service';
 import { Email } from '../valueObjects/email';
 import { EmailAddress } from '../valueObjects/emailAddress';
 describe('EmailService', () => {
+  const mailer = {
+    sendMail: jest.fn(),
+  };
+  const emailService = new EmailService(mailer);
+  it('should instantiate the class', () => {
+    expect(emailService).toBeDefined();
+  })
   it('should call sendMail with the correct params', () => {
-    const mailer = {
-      sendMail: jest.fn(),
-    };
-    const emailService = new EmailService(mailer);
     const emailAddress = new EmailAddress('test@gmail.com');
     const email = new Email(emailAddress, 'test', 'test');
     emailService.sendEmail(email);
+    expect(mailer.sendMail).toBeCalled()
   });
 });
