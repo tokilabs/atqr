66ca1054b5ded07bc6210a9653c2cd408eefc47b
Gabriela Sanches <gabi@toki.life>
2023-01-24 15:53:42 -0300

    perf(idsintefaces.ts): folder holding interfaces for ID's

---
+++ libs/atqr/domain/src/lib/services/interfaces/idsInterfaces.ts
@@ -0,0 +1,14 @@
+import { Guid } from '@tokilabs/lang';
+
+export interface ChallengeId {
+  challengeId: Guid;
+}
+export interface UserId {
+  userId: Guid;
+}
+export interface GoalId {
+  goalId: Guid;
+}
+export interface JudgeId {
+  judgeId: Guid;
+}
