7e8e99be3d666b08eaeed69fe0f9546c85ffa31d
Gabriela Sanches <gabi@toki.life>
2023-01-26 13:15:10 -0300

    refactor(valueobject.ts): vO is now updated with documentation details

--- libs/atqr/domain/src/utils/valueObject.ts
+++ libs/atqr/domain/src/utils/valueObject.ts
@@ -7,23 +7,69 @@ const Constructor = Symbol.for('plow:ValueObject.Constructor');
 /**
  * Base class for ValueObject's
  *
+ * How to extend this class:
+ * 
+ *  1. Declare all properties in the Constructor
+ *      They must all be `public readonly`
+ *  2. Call super passing the arguments:
+ *      - The instance type of the class you are creating
+ *      - An array of string with the names of the constructor parameters in the same order the constructor receives them
+ *  3. Create a `setPROP` for each property of your Value Object
+ *      - Replace PROP is the name of the function by the property name
+ *      - Use the base class' {@link newInstanceWith } method to create new instances without boilerplate
+ * 
+ * @example
+ * 
+ * <code>
+ *  class SomeVO extends ValueObject<SomeVO> {
+ *    constructor(
+ *      public readonly propA: string,
+ *      public readonly propB: number,
+ *      public readonly propC: boolean) {
+ *        super(
+ *          SomeVO, [
+ *            "propA",
+ *            "propB",
+ *            "propC"
+ *          ]
+ *        );
+ *    }
+ * 
+ *    // Set Methods
+ * 
+ *    ...other set methods
+ * 
+ *    public setPropB(propB: number) {
+ *      return this.newInstanceWith({
+ *        propB
+ *      });
+ *    }
+ *    ...other set methods
+ *  }
+ * </code>
+ * 
+ * @remarks
  * A value object is an object whose identity is
  * determined by it's properties values.
  *
- * Value objects MUST:
+ * To achieve that, Value Objects MUST:
  * - Be immutable
  * - Be compared by value-equality
+ * 
  * @export
  * @class ValueObject
  */
-export class ValueObject<TObject> {
+export abstract class ValueObject<TObject> {
   /**
-   * @param construct Your value object's constructor
-   * @param constructParams Name of the properties to pass to constructor IN ORDER
+   * See class documentation for a "how to" on extending 
+   * this class
+   * 
+   * @param construct Your Value Object's instance type
+   * @param constructParams Name of the parameters passed to your  value object's constructor IN ORDER
    */
   constructor(
     private construct: new (...args: any[]) => TObject,
-    constructParams: (keyof TObject)[]
+    constructParams:  (keyof TObject)[]
   ) {
     this[Constructor] = construct;
     this[ConstructorParams] = constructParams;
