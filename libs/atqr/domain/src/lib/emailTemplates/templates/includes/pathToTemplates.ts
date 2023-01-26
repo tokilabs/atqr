71494496a3bbb849ad6cfc433e93b1963e077519
Gabriela Sanches <gabi@toki.life>
2023-01-26 13:21:01 -0300

    style(email templates): all email templates are now in "lib/templates/email.ts"
    n

--- libs/atqr/domain/src/lib/emailTemplates/templates/includes/pathToTemplates.ts
+++
@@ -1,6 +0,0 @@
-import * as path from 'path';
-
-export const pathToTemplates = path.join(
-    process.cwd(),
-    'libs/atqr/domain/src/lib/EmailService/templates/'
-  );
---
+++ libs/atqr/domain/src/lib/types/notification.types.ts
@@ -0,0 +1,32 @@
+import { ITemplateDataTypes } from '../services/notification/templateDataType.interface';
+
+export type TemplateDataTypes = {
+  [K in keyof ITemplateDataTypes]: ITemplateDataTypes[K];
+};
+
+export enum MessageTemplateId {
+  ChallengeCreated = 'ChallengeCreated',
+  ChallengeCanceled = 'ChallengeCanceled',
+  ChallengeOngoing = 'ChallengeOngoing',
+  ChallengeCompleted = 'ChallengeCompleted',
+  ChallengeFailed = 'ChallengeFailed',
+  YourOfficiationHaveBeenRequested = 'YourOfficiationHaveBeenRequested',
+  YourOfficiationRequestHaveBeenAccepted = 'YourOfficiationRequestHaveBeenAccepted',
+  YourOfficiationRequestHaveBeenRejected = 'YourOfficiationRequestHaveBeenRejected',
+  YourOfficiationRequestHaveBeenIgnored = 'YourOfficiationRequestHaveBeenIgnored',
+  YouHaveBeenChallenged = 'YouHaveBeenChallenged',
+  YourContenderAcceptedTheChallenge = 'YourContenderAcceptedTheChallenge',
+  YourContenderRejectedTheChallenge = 'YourContenderRejectedTheChallenge',
+  YourContenderIgnored = 'YourContenderIgnored',
+  YourContenderRemovedYouFromJudgeRole = 'YourContenderRemovedYouFromJudgeRole',
+  YourContenderCanceledTheChallenge = 'YourContenderCanceledTheChallenge',
+  YourContenderWereTheySuccessful = 'YourContenderWereTheySuccessful',
+  ReminderToOfficiate = 'ReminderToOfficiate',
+  YourJudgeDidNotAnswer = 'YourJudgeDidNotAnswer',
+}
+
+export enum NotificationChannel {
+  Email = 'Email',
+  SMS = 'SMS',
+  Push = 'Push',
+}
