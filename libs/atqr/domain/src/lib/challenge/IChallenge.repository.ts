c19ef119b70a1f7b88279d9160e3e9df00cbf3c6
Alberto Nunes <bisneto.alberto@gmail.com>
2023-01-30 14:56:05 -0300

    feat: wIP: need more typing

--- libs/atqr/domain/src/lib/challenge/IChallenge.repository.ts
+++ libs/atqr/domain/src/lib/challenge/IChallenge.repository.ts
@@ -1,9 +1,15 @@
 import { Guid } from '@tokilabs/lang';
-import { Challenge } from '.';
+import { Challenge } from './';
 import { Player } from '../player';
-
+import { Invitee } from '../valueObjects';
+import {
+  ContenderOutcome,
+  ParticipationStatus,
+  JudgmentStatus,
+} from '../types';
+import { PaymentStatus, PaymentFundsStatus } from '../PaymentMethod/enums';
 export interface IChallengeRepository {
-  create(data: Challenge);
+  create(data: Challenge): Promise<void>;
 
   ContenderCreateChallenge(challenge: Challenge): Promise<void>;
   JudgeCreateTheChallenge(challenge: Challenge): Promise<void>;
@@ -19,21 +25,39 @@ export interface IChallengeRepository {
   addContenderToChallenge(challengeId: Guid, contender: Player): Promise<void>;
   addJudgeToChallenge(challengeId: Guid, judge): Promise<void>;
 
-  addInviteeToChallenge(challengeId: Guid, invitee): Promise<void>;
+  addInviteeToChallenge(challengeId: Guid, invitee: Invitee): Promise<void>;
 
-  inviteeToParticipateAccepted(id: Guid, inviteeStatus): Promise<void>;
-  inviteeToParticipateRejected(id: Guid, inviteeStatus): Promise<void>;
-  inviteeToParticipateCanceled(id: Guid, inviteeStatus): Promise<void>;
+  inviteeToParticipateAccepted(
+    id: Guid,
+    inviteeStatus: ParticipationStatus
+  ): Promise<void>;
+  inviteeToParticipateRejected(
+    id: Guid,
+    inviteeStatus: ParticipationStatus
+  ): Promise<void>;
+  inviteeToParticipateCanceled(
+    id: Guid,
+    inviteeStatus: ParticipationStatus
+  ): Promise<void>;
 
-  judgeWasAdded(id: Guid, judgeStatus): Promise<void>;
-  judgeOfficiateTheChallenge(id: Guid, judgeStatus): Promise<void>;
+  judgeWasAdded(id: Guid, judgeStatus: JudgmentStatus): Promise<void>;
+  judgeOfficiateTheChallenge(
+    id: Guid,
+    judgeStatus: JudgmentStatus
+  ): Promise<void>;
 
-  judgmentStatusWasSuccess(id: Guid, judgmentStatus): Promise<void>;
-  judgmentStatusWasFailed(id: Guid, judgmentStatus): Promise<void>;
+  judgmentStatusWasSuccess(
+    id: Guid,
+    judgmentStatus: JudgmentStatus
+  ): Promise<void>;
+  judgmentStatusWasFailed(
+    id: Guid,
+    judgmentStatus: JudgmentStatus
+  ): Promise<void>;
 
   updatePaymentFundsStatusToUnverified(
     id: Guid,
-    paymentFundStatus
+    paymentFundStatus: PaymentFundsStatus
   ): Promise<void>;
   updatePaymentFundsStatusToAuthorized(
     id: Guid,
@@ -41,17 +65,44 @@ export interface IChallengeRepository {
   ): Promise<void>;
   updatePaymentFundsStatusToDenied(id: Guid, paymentFundStatus): Promise<void>;
 
-  updatePaymentStatusToDefaulted(id: Guid, paymentStatus): Promise<void>;
-  updatePaymentStatusToFailed(id: Guid, paymentStatus): Promise<void>;
-  updatePaymentStatusToForgiven(id: Guid, paymentStatus): Promise<void>;
-  updatePaymentStatusToPaid(id: Guid, paymentStatus): Promise<void>;
-  updatePaymentStatusToPending(id: Guid, paymentStatus): Promise<void>;
-  updatePaymentStatusToRefunded(id: Guid, paymentStatus): Promise<void>;
-  updatePaymentStatusToRefundRequested(id: Guid, paymentStatus): Promise<void>;
+  updatePaymentStatusToDefaulted(
+    id: Guid,
+    paymentStatus: PaymentStatus
+  ): Promise<void>;
+  updatePaymentStatusToFailed(
+    id: Guid,
+    paymentStatus: PaymentStatus
+  ): Promise<void>;
+  updatePaymentStatusToForgiven(
+    id: Guid,
+    paymentStatus: PaymentStatus
+  ): Promise<void>;
+  updatePaymentStatusToPaid(
+    id: Guid,
+    paymentStatus: PaymentStatus
+  ): Promise<void>;
+  updatePaymentStatusToPending(
+    id: Guid,
+    paymentStatus: PaymentStatus
+  ): Promise<void>;
+  updatePaymentStatusToRefunded(
+    id: Guid,
+    paymentStatus: PaymentStatus
+  ): Promise<void>;
+  updatePaymentStatusToRefundRequested(
+    id: Guid,
+    paymentStatus: PaymentStatus
+  ): Promise<void>;
 
-  contenderStillPlaying(id: Guid, contenderOutcome): Promise<void>;
-  contenderSucceeded(id: Guid, contenderOutcome): Promise<void>;
-  contenderFailed(id: Guid, contenderOutcome): Promise<void>;
+  contenderStillPlaying(
+    id: Guid,
+    contenderOutcome: ContenderOutcome
+  ): Promise<void>;
+  contenderSucceeded(
+    id: Guid,
+    contenderOutcome: ContenderOutcome
+  ): Promise<void>;
+  contenderFailed(id: Guid, contenderOutcome: ContenderOutcome): Promise<void>;
 
   recordChallengeSuccess(id: Guid): Promise<void>;
   recordChallengeFailure(id: Guid): Promise<void>;
