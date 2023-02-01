e8383ffdd9323b7a44f631f5286797de28ba7125
Alberto Nunes <bisneto.alberto@gmail.com>
2023-02-01 11:45:11 -0300

    test: improve the tests

--- libs/atqr/domain/src/lib/services/deadlineMonitor/deadlineMonitor.service.test.ts
+++ libs/atqr/domain/src/lib/services/deadlineMonitor/deadlineMonitor.service.test.ts
@@ -1,27 +1,16 @@
-import { DeadLineMonitorService } from './deadlineMonitor.service';
+import { DeadlineMonitorService } from './';
 import { IChallengeRepository } from '../../challenge';
 import cron from 'cron-validate';
 import { CronJob } from 'cron';
-import { dateDiff } from '../../../utils/dateDifference';
 
 describe('DeadlineMonitorService', () => {
-  const challengeRepository = {} as IChallengeRepository;
-  const mockAnewDayArrived = jest.fn();
-
-  const date = '* 0 0 * * *';
-  const cronJob = new CronJob(
-    date,
-    mockAnewDayArrived,
-    null,
-    true,
-    'America/Sao_Paulo'
-  );
-
-  it('should be defined', () => {
-    expect(new DeadLineMonitorService(challengeRepository)).toBeDefined();
+  it('Service should be defined', () => {
+    const challengeRepository = {} as IChallengeRepository;
+    const service = new DeadlineMonitorService(challengeRepository);
+    expect(service).toBeInstanceOf(DeadlineMonitorService);
   });
 
-  it('should be valid', () => {
+  it('Cron date should be valid', () => {
     expect(
       cron('* * * * * *', {
         preset: 'npm-cron-schedule',
@@ -29,24 +18,31 @@ describe('DeadlineMonitorService', () => {
     ).toBe(true);
   });
 
-  it('should be invalid', () => {
+  it('Cron date should be invalid', () => {
     expect(
       cron('t * e s * t ', {
         preset: 'npm-cron-schedule',
       }).isValid()
     ).toBe(false);
   });
-  // it('Dates should not be the same', () => {
-  //   const oldDate = new Date();
-  //   oldDate.setFullYear(2022);
-  //   const newDate = new Date();
-  //   expect(dateDiff(oldDate, newDate)).toBeGreaterThan(0);
-  // });
 
-  // it('should be instanceOf', () => {
-  //   expect(cronJob).toBeInstanceOf(CronJob);
-  // });
-  it('CronJob should call method aNewDayArrived', () => {
-    expect(mockAnewDayArrived).toBeCalled()
+  it('Cronjob should work', () => {
+    const mockFn = jest.fn().mockImplementation(() => {
+      return 'test';
+    });
+
+    const date = '0 * * * * *';
+
+    if (
+      cron(date, {
+        preset: 'npm-cron-schedule',
+      }).isValid()
+    ) {
+      new CronJob(date, mockFn(), null, false, 'America/Sao_Paulo');
+    } else {
+      throw new Error(cron(date).getError().join(' '));
+    }
+
+    expect(mockFn).toHaveBeenCalled();
   });
 });
