4a5ea8c175610aeac3990225783446c095c433a3
Alberto Nunes <bisneto.alberto@gmail.com>
2023-02-02 10:38:28 -0300

    feat: all necessary methods are in the interface

--- libs/atqr/domain/src/lib/challenge/IChallenge.repository.ts
+++ libs/atqr/domain/src/lib/challenge/IChallenge.repository.ts
@@ -1,116 +1,46 @@
 import { Guid } from '@tokilabs/lang';
 import { Challenge } from './';
-import { Player } from '../player';
-import { Invitee } from '../valueObjects';
-import {
-  ContenderOutcome,
-  ParticipationStatus,
-  JudgmentStatus,
-} from '../types';
-import { PaymentStatus, PaymentFundsStatus } from '../PaymentMethod/enums';
+import { Email } from '../valueObjects';
 export interface IChallengeRepository {
-  create(data: Challenge): Promise<void>;
+  ContenderCreatesTheChallenge(challenge: Challenge): Promise<void>;
+  JudgeCreatesTheChallenge(challenge: Challenge): Promise<void>;
 
-  ContenderCreateChallenge(challenge: Challenge): Promise<void>;
-  JudgeCreateTheChallenge(challenge: Challenge): Promise<void>;
+  findChallengeById(id: Guid): Promise<Challenge>;
+  findChallengeByEmail(email: Email): Promise<Challenge>;
+  findLastChallenges(): Promise<Challenge>;
+  findManyChallenges(amount: number): Promise<Challenge[]>;
+  findOverdueChallenges(): Promise<Challenge[]>;
 
-  read(id: Guid, fieldsToRead: Partial<Challenge>): Promise<Challenge[]>;
-
-  findChallengeById(id: Guid): Promise<Challenge[]>;
-  getLatestChallengesCreated(amount: number): Promise<Challenge[]>;
-  getOverdueChallenges(): Promise<Challenge[]>;
-
-  update(id: Guid, fieldsToUpdate: Partial<Challenge>): Promise<void>;
-
-  addContenderToChallenge(challengeId: Guid, contender: Player): Promise<void>;
-  addJudgeToChallenge(challengeId: Guid, judge): Promise<void>;
-
-  addInviteeToChallenge(challengeId: Guid, invitee: Invitee): Promise<void>;
-
-  inviteeToParticipateAccepted(
+  challengeStatusChangedToOngoing(id: Guid, challenge: Challenge): Promise<void>;
+  challengeStatusChangedToOverdue(id: Guid, challenge: Challenge): Promise<void>;
+  challengeStatusChangedToAbandoned(
     id: Guid,
-    inviteeStatus: ParticipationStatus
+    challenge: Challenge
   ): Promise<void>;
-  inviteeToParticipateRejected(
+  challengeStatusChangedToCanceled(
     id: Guid,
-    inviteeStatus: ParticipationStatus
+    challenge: Challenge
   ): Promise<void>;
-  inviteeToParticipateCanceled(
+  challengeStatusChangedToFinished(
     id: Guid,
-    inviteeStatus: ParticipationStatus
+    challenge: Challenge
   ): Promise<void>;
 
-  judgeWasAdded(id: Guid, judgeStatus: JudgmentStatus): Promise<void>;
-  judgeOfficiateTheChallenge(
+  inviteeAcceptsPlayChallengeAsContender(
     id: Guid,
-    judgeStatus: JudgmentStatus
+    challenge: Challenge
   ): Promise<void>;
+  inviteeAcceptsPlayChallengeAsJudge(
+    id: Guid,
+    challenge: Challenge
+  ): Promise<void>;
+  ownerAddsNewInvitee(id: Guid, challenge: Challenge): Promise<void>;
+  ownerAddsPaymentMethod(id: Guid, challenge: Challenge): Promise<void>;
 
-  judgmentStatusWasSuccess(
+  contenderRemovedTheJudgeFromChallenge(
     id: Guid,
-    judgmentStatus: JudgmentStatus
+    challenge: Challenge
   ): Promise<void>;
-  judgmentStatusWasFailed(
-    id: Guid,
-    judgmentStatus: JudgmentStatus
-  ): Promise<void>;
-
-  updatePaymentFundsStatusToUnverified(
-    id: Guid,
-    paymentFundStatus: PaymentFundsStatus
-  ): Promise<void>;
-  updatePaymentFundsStatusToAuthorized(
-    id: Guid,
-    paymentFundStatus
-  ): Promise<void>;
-  updatePaymentFundsStatusToDenied(id: Guid, paymentFundStatus): Promise<void>;
-
-  updatePaymentStatusToDefaulted(
-    id: Guid,
-    paymentStatus: PaymentStatus
-  ): Promise<void>;
-  updatePaymentStatusToFailed(
-    id: Guid,
-    paymentStatus: PaymentStatus
-  ): Promise<void>;
-  updatePaymentStatusToForgiven(
-    id: Guid,
-    paymentStatus: PaymentStatus
-  ): Promise<void>;
-  updatePaymentStatusToPaid(
-    id: Guid,
-    paymentStatus: PaymentStatus
-  ): Promise<void>;
-  updatePaymentStatusToPending(
-    id: Guid,
-    paymentStatus: PaymentStatus
-  ): Promise<void>;
-  updatePaymentStatusToRefunded(
-    id: Guid,
-    paymentStatus: PaymentStatus
-  ): Promise<void>;
-  updatePaymentStatusToRefundRequested(
-    id: Guid,
-    paymentStatus: PaymentStatus
-  ): Promise<void>;
-
-  contenderStillPlaying(
-    id: Guid,
-    contenderOutcome: ContenderOutcome
-  ): Promise<void>;
-  contenderSucceeded(
-    id: Guid,
-    contenderOutcome: ContenderOutcome
-  ): Promise<void>;
-  contenderFailed(id: Guid, contenderOutcome: ContenderOutcome): Promise<void>;
-
-  recordChallengeSuccess(id: Guid): Promise<void>;
-  recordChallengeFailure(id: Guid): Promise<void>;
-  recordChallengeCanceled(id: Guid): Promise<void>;
-
-  delete(id: Guid, fieldsToDelete: Partial<Challenge>): Promise<void>;
-
-  removeJudgeFromChallenge(challengeId: Guid, judge): Promise<void>;
 }
 
 export const IChallengeRepository = Symbol.for('IChallengeRepository');
